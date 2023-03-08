9189568b113a189d414c66161789210825615d77
vpalex999 <vpalex@mail.ru>
2023-03-08 11:09:58 +0500

    listing2.18

---
+++ stage2/listing2-18.py
@@ -0,0 +1,22 @@
+""" Листинг 2.18 Попытка конкурентного выполнения счетного кода."""
+import asyncio
+from util.async_timer import async_timed
+from util.delay_functions import delay
+
+
+@async_timed()
+async def cpu_bound_work() -> int:
+    counter = 0
+    for i in range(100000000):
+        counter = counter + 1
+    return counter
+
+
+@async_timed()
+async def main():
+    task_one = asyncio.create_task(cpu_bound_work())
+    task_two = asyncio.create_task(cpu_bound_work())
+    await task_one
+    await task_two
+
+asyncio.run(main())
