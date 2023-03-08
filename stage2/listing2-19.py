57a2fc5c10b7c97732314925caa2dea4c407742a
vpalex999 <vpalex@mail.ru>
2023-03-08 11:16:23 +0500

    listing2.19

---
+++ stage2/listing2-19.py
@@ -0,0 +1,24 @@
+""" Листинг 2.19 Счетный код и длительная задача."""
+import asyncio
+from util.delay_functions import delay
+from util.async_timer import async_timed
+
+
+@async_timed()
+async def cpu_bound_work() -> int:
+    counter = 0
+    for i in range(100000000):
+        counter = counter + 1
+    return counter
+
+
+@async_timed()
+async def main():
+    task_one = asyncio.create_task(cpu_bound_work())
+    task_two = asyncio.create_task(cpu_bound_work())
+    delay_task = asyncio.create_task(delay(4))
+    await task_one
+    await task_two
+    await delay_task
+
+asyncio.run(main())
