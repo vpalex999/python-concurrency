b70c38ae7ab6772b6bf441c2cdacf74cb64ce91c
vpalex999 <vpalex@mail.ru>
2023-03-08 20:51:25 +0500

    listing3.1

---
+++ stage3/listing3-1.py
@@ -0,0 +1,13 @@
+""" Листинг 3.1 Запуск сервера и прослушивание порта для подключения."""
+import socket
+
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+server_address = ("127.0.0.1", 8000)
+server_socket.bind(server_address)
+server_socket.listen()
+
+connection, client_address = server_socket.accept()
+print(f"Получен запрос на подключение от {client_address}!")
