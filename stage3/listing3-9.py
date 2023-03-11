60f60616ddc0cf5bad21ba6a0e5607b4df9f26bf
vpalex999 <vpalex@mail.ru>
2023-03-11 07:24:35 +0500

    listing3.9

---
+++ stage3/listing3-9.py
@@ -0,0 +1,23 @@
+""" Листинг 3.9 Добавление обработчика сигнала, снимающего все задачи."""
+import asyncio
+import signal
+from asyncio import AbstractEventLoop
+from typing import Set
+
+from util.delay_functions import delay
+
+
+def cancel_tasks():
+    print("Получен сигнал SIGINT!")
+    tasks: Set[asyncio.Task] = asyncio.all_tasks()
+    print(f"Снимается {len(tasks)} задач.")
+    [task.cancel() for task in tasks]
+
+
+async def main():
+    loop: AbstractEventLoop = asyncio.get_running_loop()
+    loop.add_signal_handler(signal.SIGINT, cancel_tasks)
+
+    await delay(10)
+
+asyncio.run(main())
---
+++ stage3/util/delay_functions.py
@@ -0,0 +1,9 @@
+""" Модуль вспомогательных утилит."""
+import asyncio
+
+
+async def delay(delay_seconds: int) -> int:
+    print(f"засыпаю на {delay_seconds} с.")
+    await asyncio.sleep(delay_seconds)
+    print(f"сон в течение {delay_seconds} закончился.")
+    return delay_seconds
