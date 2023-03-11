b01e414ab4fd3b0f648daf34adc36f2a1c120abe
vpalex999 <vpalex@mail.ru>
2023-03-11 08:28:41 +0500

    listing3.10

---
+++ stage3/listing3-10.py
@@ -0,0 +1,75 @@
+""" Листинг 3.10 Корректная остановка эхо-сервера."""
+import asyncio
+import socket
+from asyncio import AbstractEventLoop
+import logging
+import signal
+from typing import List
+
+
+async def echo(connection: socket.socket, loop: AbstractEventLoop) -> None:
+    try:
+        while data := await loop.sock_recv(connection, 1024):
+            print("Получены данные!")
+            if data == b'boom\r\n':
+                raise Exception("Неожиданная ошибка сети.")
+            await loop.sock_sendall(connection, data)
+    except Exception as ex:
+        logging.exception(ex)
+    finally:
+        connection.close()
+
+echo_tasks: List[asyncio.Task] = []
+
+
+async def listen_for_connection(server_socket: socket.socket, loop: AbstractEventLoop):
+    while True:
+        connection, address = await loop.sock_accept(server_socket)
+        connection.setblocking(False)
+        print(f"Получен запрос на подключение от {address}")
+        echo_task = asyncio.create_task(echo(connection, loop))
+        echo_tasks.append(echo_task)
+
+
+class GracefulExit(SystemExit):
+    pass
+
+
+def shutdown():
+    raise GracefulExit()
+
+
+async def close_echo_tasks(echo_tasks: List[asyncio.Task]):
+    print("\nstart close_echo_tasks")
+    waiters = [asyncio.wait_for(task, 2) for task in echo_tasks]
+    for task in waiters:
+        try:
+            await task
+        except asyncio.exceptions.TimeoutError:
+            # Здесь мы ожидаем истечения тайм-аута
+            print("Здесь мы ожидаем истечения тайм-аута по задаче")
+            pass
+
+
+async def main(loop: AbstractEventLoop):
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    server_address = ("127.0.0.1", 8000)
+    server_socket.setblocking(False)
+    server_socket.bind(server_address)
+    server_socket.listen()
+
+    for signame in {"SIGINT", "SIGTERM"}:
+        loop.add_signal_handler(getattr(signal, signame), shutdown)
+
+    await listen_for_connection(server_socket, asyncio.get_event_loop())
+
+
+loop = asyncio.new_event_loop()
+try:
+    loop.run_until_complete(main(loop))
+except GracefulExit:
+    loop.run_until_complete(close_echo_tasks(echo_tasks))
+finally:
+    loop.close()
