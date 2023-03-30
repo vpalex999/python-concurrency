0dfdbc883f2669e1180811284b6d90829f62638e
vpalex999 <vpalex@mail.ru>
2023-03-30 17:29:20 +0500

    listing5.7

---
+++ stage5/listing5_7.py
@@ -0,0 +1,32 @@
+""" Листинг 5.7 Создание пула подключений и конкурентное выполнение запросов."""
+import asyncio
+import asyncpg
+
+
+product_query = \
+    """
+SELECT
+p.product_id,
+p.product_name,
+p.brand_id,
+s.sku_id,
+pc.product_color_name,
+ps.product_size_name
+FROM product as p
+JOIN sku as s on s.product_id = p.product_id
+JOIN product_color as pc on pc.product_color_id = s.product_color_id
+JOIN product_size as ps on ps.product_size_id = s.product_size_id
+WHERE p.product_id = 100
+"""
+
+
+async def query_product(pool: asyncpg.Pool):
+    async with pool.acquire() as connection:
+        return await connection.fetchrow(product_query)
+
+
+async def main():
+    async with asyncpg.create_pool(host="127.0.0.1", port=5432, user="postgres", database="products", password="password", min_size=6, max_size=6) as pool:
+        await asyncio.gather(query_product(pool), query_product(pool))
+
+asyncio.run(main())
