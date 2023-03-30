df5ef78116ccafa1d1895a9650dbc7dd4640976f
vpalex999 <vpalex@mail.ru>
2023-03-30 18:02:03 +0500

    listing5.15

---
+++ stage5/listing5_15.py
@@ -0,0 +1,17 @@
+""" Листинг 5.15 Потоковая обработка результатов."""
+import asyncpg
+import asyncio
+
+
+async def main():
+    connection = await asyncpg.connect(host="127.0.0.1", port=5432, user="postgres", database="products", password="password")
+
+    query = "SELECT product_id, product_name FROM product"
+
+    async with connection.transaction():
+        async for product in connection.cursor(query):
+            print(product)
+
+    await connection.close()
+
+asyncio.run(main())
