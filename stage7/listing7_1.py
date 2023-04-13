aca21446ba1567feeffd7c5b1a074cb5fb3fceb7
vpalex999 <vpalex@mail.ru>
2023-04-13 19:20:11 +0500

    listing7.1

---
+++ stage7/listing7_1.py
@@ -0,0 +1,22 @@
+""" Листинг 7.1 Многопоточный эхо-сервер."""
+from http import server
+from threading import Thread
+import socket
+
+
+def echo(client: socket.socket):
+    while True:
+        data = client.recv(2048)
+        print(f"Получено {data}, отправляю!")
+        client.sendall(data)
+
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.bind(("127.0.0.1", 8000))
+    server.listen()
+
+    while True:
+        connection, _ = server.accept()
+        thread = Thread(target=echo, args=(connection,))
+        thread.start()
