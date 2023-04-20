7896f438d597b3e409693019d47df83af243d9fe
vpalex999 <vpalex@mail.ru>
2023-04-20 17:02:16 +0500

    listing7.3

---
+++ stage7/listing7_3.py
@@ -0,0 +1,12 @@
+""" Листинг 7.3 Базовое использование requests."""
+import requests
+
+
+def get_status_code(url: str) -> int:
+    response = requests.get(url)
+    return response.status_code
+
+
+url = "https://www.example.com"
+print(get_status_code(url))
+print(get_status_code(url))
